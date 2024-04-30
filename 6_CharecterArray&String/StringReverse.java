f4b5529db050242ff5c1f005bace92d296aeb5bc
progdeep <120062220+CoadingRaja@users.noreply.github.com>
2024-04-30 16:22:13 +0530

    Reverse of a string

--- 6_CharecterArray&String/StringReverse.java
+++ 6_CharecterArray&String/StringReverse.java
@@ -1,1 +1,18 @@
-import java.util.
+import java.util.Scanner;
+public class StringReverse{
+    String str;
+    StringReverse(){
+        System.out.print("Enter your String"+" ");
+        Scanner in=new Scanner(System.in);
+        this.str=in.nextLine();
+        in.close();
+    }
+    public static void main(String a[]){
+        StringReverse ob= new StringReverse();
+        char[] arr=ob.str.toCharArray();
+        for(int i=ob.str.length()-1;i>=0;i--)
+            System.out.print(arr[i]);
+       
+    }
+    
+}
